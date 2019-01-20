30d3f2119227cc9ec0f605cca607cd17ff4cf015
katsumin <katsumi.noguchi@gmail.com>
2019-01-20 19:50:31 +0900

    add json parse logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,51 @@
+#include <ArduinoJson.h>
 #include <SD.h>
 #include <Arduino.h>
 #include "PLUSEncoder.h"
+
+const char *filename = "/remocon.json";
+void loadConfig()
+{
+  if (!SD.exists(filename))
+  {
+    Serial.println("no file!");
+    return;
+  }
+  File file = SD.open(filename);
+  DynamicJsonBuffer jsonBuffer;
+
+  // Parse the root object
+  JsonObject &root = jsonBuffer.parseObject(file);
+
+  if (root.success())
+  {
+    Serial.println(root.size());
+    for (auto kv : root)
+    {
+      Serial.println(kv.key);
+      JsonObject &o = root[kv.key];
+      const char *format = o["format"];
+      const char *custom = o["custom"];
+      Serial.printf(" format:%s, customCode:%s\n", format, custom);
+      JsonObject &buttons = o["buttons"];
+      Serial.println(" buttons");
+      for (auto kv2 : buttons)
+      {
+        Serial.printf("  name:%s -> data:%s\n", kv2.key, kv2.value.as<char *>());
+      }
+    }
+  }
+  else
+  {
+    Serial.println(F("Failed to read file, using default configuration"));
+  }
+}
 
 void setup()
 {
   M5.begin();
   Wire.begin();
+  loadConfig();
 }
 
 void loop()
